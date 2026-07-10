5c2328fadf71fb5cee0144d7cc4a95c28c567813
江月夜 <615944323@qq.com>
2026-07-10 11:15:53 +0800

    chore: pkg update

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import ajiu9 from '@ajiu9/eslint-config';
+
+export default ajiu9(
+  {
+    typescript: false,
+    jsx: false,
+  },
+  {
+    rules: {
+      'no-bitwise': 'off',
+      'no-lonely-if': 'off',
+      'class-methods-use-this': 'off',
+      'arrow-body-style': 'off',
+      'no-loop-func': 'off',
+    },
+  },
+);
